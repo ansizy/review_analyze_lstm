57bdbee3f21e18520df74407d8a0aefffb99adc8
ansizy <2768592469@qq.com>
2026-03-25 13:18:21 +0800

    增加GRU模型实现

--- src/model.py
+++ src/model.py
@@ -32,3 +32,32 @@ class ReviewAnalyzeModel(nn.Module):
         # output shape [batch_size]
 
         return output
+
+class ReviewAnalyzeModelGRU(nn.Module):
+    def __init__(self, vocab_size, padding_index):
+        super().__init__()
+
+        # embedding lstm linear
+        # padding_idx 填充字符的index
+        self.embedding = nn.Embedding(num_embeddings=vocab_size, embedding_dim=config.EMBEDDING_DIM, padding_idx=padding_index)
+
+        self.gru = nn.GRU(input_size=config.EMBEDDING_DIM, hidden_size=config.HIDDEN_SIZE, batch_first=True)
+
+        self.linear = nn.Linear(in_features=config.HIDDEN_SIZE, out_features=1)
+
+    def forward(self, x):
+        # x shape [batch_size, seq_len]
+        embedded = self.embedding(x)
+        # embedded shape [batch_size, seq_len, embedding_dim]
+        gru_out, _ = self.gru(embedded)
+        # gru_out shape [batch_size, seq_len, hidden_size]
+        # 取最后一个非 pad 的隐藏状态
+        # last_hidden_state = gru_out[:, -1, :]
+        batch_indexes = torch.arange(0, gru_out.shape[0])
+        lengths = (x != self.embedding.padding_idx).sum(dim=1)
+        last_hidden_state = gru_out[batch_indexes, lengths - 1]
+
+        output = self.linear(last_hidden_state).squeeze(-1)
+        # output shape [batch_size]
+
+        return output
